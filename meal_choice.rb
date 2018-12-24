0c1a143ed73e0cb355d0dfabc622025f8ca07554
mooting <justin_w_davis@yahoo.com>
2018-12-24 10:42:30 -0800

    lab solved

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,2 +1,4 @@
-# Define a method meal_choice that returns the meal_choice that was
+def meal_choice(choice = 'meat')
+  choice
+end# Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
